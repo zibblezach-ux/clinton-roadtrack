7a439039529cc1e7fb5b27a137388cb631d17119
zibblezach-ux <zibblezach@gmail.com>
2026-05-02 11:10:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -247,7 +247,9 @@ def seed():
         db.session.commit()
     print("Seed data loaded.")
 
+@app.before_request
+def create_tables():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
